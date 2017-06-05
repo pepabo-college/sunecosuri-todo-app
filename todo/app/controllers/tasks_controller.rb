58c3edc09b1c290e9e35c82de94ec58c135b63f0
sunecosuri <sunecosuri@pepabo.com>
2017-06-05 15:54:32 +0900

    Jbuilderの実装

--- todo/app/controllers/tasks_controller.rb
+++ todo/app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task,  only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
@@ -17,23 +17,27 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
-    if @task.save
-      format.html { redirect_to @task, notice: 'Task was successfully created.' }
-      format.json { render :show, status: :created, location: @task }
-    else
-      format.html { render :new }
-      format.json { render json: @task.errors, status: :unprocessable_entity }
+
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to @task, notice: 'Task was successfully created.' }
+        format.json { render :show, status: :created, location: @task }
+      else
+        format.html { render :new }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def update
     respond_to do |format|
-    if @task.update(task_params)
-      format.html { redirect_to @task, notice: 'Task was successfully updated.' }
-      format.json { render :show, status: :ok, location: @task }
-    else
-      format.html { render :edit }
-      format.json { render json: @task.errors, status: :unprocessable_entity }
+      if @task.update(task_params)
+        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
+        format.json { render :show, status: :ok, location: @task }
+      else
+        format.html { render :edit }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
     end
   end
 
