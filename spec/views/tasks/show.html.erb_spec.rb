19bb5889b86cd823a2768a38cc01d51018b21bef
sunecosuri <sunecosuri@pepabo.com>
2017-06-13 17:55:28 +0900

    Capybaraの設定の記述とテストコードの追加

--- spec/views/tasks/show.html.erb_spec.rb
+++ spec/views/tasks/show.html.erb_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "tasks/show", type: :view do
   context '存在しないIDでアクセスしようとしたとき' do
-    render
+    visit '/tasks/show/99999'
     expect(rendered).to raise_error(ActiveRecord::RecordNotFound)
   end
 end
